aa17bcde5823779ceff0aaa0b39a1d676fcc1832
monikatrix <kskm4789@gmail.com>
2025-04-08 19:57:35 +0530

    Replaced real key with placeholder

--- src/main/java/com/example/OnlineFoodOrdering/config/JwtConstant.java
+++ src/main/java/com/example/OnlineFoodOrdering/config/JwtConstant.java
@@ -1,6 +1,6 @@
 package com.example.OnlineFoodOrdering.config;
 
 public class JwtConstant {
-    public static final String SECRET_KEY = "gufwhquobnwbiebnonbpnbdnbsonbonbnbobneonewwouhuhvhuheihvibidndnbenbpnepbnnbms";
+    public static final String SECRET_KEY = "your key";
     public static final String JWT_HEADER = "Authorization";
 }
